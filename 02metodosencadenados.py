65fea716aca79cab5eb5c888738f766ec9bece45
Jose Hermosilla <jose.hermosillaasach.cl>
2021-06-16 21:57:19 -0400

    se agregan los cambios a 02metodosencadenados, faltaria agregar el log para tener el registro

--- 02metodosencadenados.py
+++ 02metodosencadenados.py
@@ -7,20 +7,27 @@ class User:
 # agrega el método deposit 
     def make_deposit(self, amount):	# toma un argumento que es el monto del depósito
         self.account_balance += amount	# la cuenta del usuario específico aumenta por la cantidad del valor recibido
+        print(f"{self.name} realizo un deposito de {amount}")
         return self
     
     def make_withdrawal(self,amount): #toma un argumento que es el monto del retiro
-        self.account_balance -= amount #la cuenta del usuario especifico disminuye por la cantidad
+        if self.account_balance-amount < 0:
+            print(f"{self.name} no tiene fondos suficientes para el retiro, SALDO ACTUAL : {self.account_balance}")
+            
+        else:  
+            self.account_balance -= amount #la cuenta del usuario especifico disminuye por la cantidad
         return self
-    
+
     def display_user_balance(self): #toma el nombre de la instancia y muestra su cuenta bancaria
-        print(self.account_balance)
+        print(f"el saldo en su cuenta es de : {self.account_balance}")
         return self
     
     def transfer_money(self, other_user,amount):
-        self.account_balance -= amount
-        other_user.account_balance += amount
-        return self
+        if self.account_balance - amount > 0:
+                self.account_balance -= amount
+                other_user.account_balance += amount
+        else:
+            print(f"{self.name} no tiene fondos suficientes para la transferencia, SALDO ACTUAL : {self.account_balance}")
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
